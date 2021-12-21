ace187b3776dedf0e7cfcf9ff3f98fb95c2dd0b0
Andrzej Rawski <rawski.andrzej@gmail.com>
2021-12-21 21:38:47 +0100

    Initial project add. Closes #1

---
+++ src/test/java/pl/antygravity/microbeerservice/MicroBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.antygravity.microbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
