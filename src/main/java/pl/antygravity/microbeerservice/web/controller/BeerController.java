051a916a0c1e02a78fa50723730970f21f9ab4b0
Andrzej Rawski <rawski.andrzej@gmail.com>
2022-01-25 15:30:39 +0100

    Added Error Handler. Closes #9

--- src/main/java/pl/antygravity/microbeerservice/web/controller/BeerController.java
+++ src/main/java/pl/antygravity/microbeerservice/web/controller/BeerController.java
@@ -2,11 +2,14 @@ package pl.antygravity.microbeerservice.web.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import pl.antygravity.microbeerservice.web.model.BeerDto;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
+@Validated
 @RequestMapping("/api/v1/beer")
 @RestController
 public class BeerController {
@@ -19,14 +22,14 @@ public class BeerController {
     }
 
     @PostMapping
-    public ResponseEntity saveNewBeer(@RequestBody BeerDto beerDto){
+    public ResponseEntity saveNewBeer(@Valid @RequestBody BeerDto beerDto){
 
         //todo impl
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @PutMapping("/{beerId}")
-    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto){
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @Valid @RequestBody BeerDto beerDto){
 
         //todo impl
         return new ResponseEntity(HttpStatus.NO_CONTENT);
